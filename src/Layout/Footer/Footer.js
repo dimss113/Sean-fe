6ca6daade6262b7f8a1acd994ea75800f43de4cc
dimss113 <dimasfadilah20@gmail.com>
2023-09-07 03:49:37 +0700

    feat: fix all problem

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -1,23 +1,24 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="bg-purp flex flex-col justify-center px-[135px]">
-    <div className="flex mt-[80px] mb-[80px]">
-      <div className="w-[391px] h-[192px] rounded-l-lg border-[1px] border-second">
-        <div className="flex flex-col">
-          <div className="flex ml-[48px] mt-[32px] gap-4 items-center">
-            <img src="/Images/phone.png" alt="" />
-            <h1 className="uppercase text-[#EF6D58] font-poppins font-normal leading-8 text-base">
-              Phone
+  return (
+    <div className="bg-purp flex flex-col justify-center px-[135px]">
+      <div className="flex mt-[80px] mb-[80px]">
+        <div className="w-[391px] h-[192px] rounded-l-lg border-[1px] border-second">
+          <div className="flex flex-col">
+            <div className="flex ml-[48px] mt-[32px] gap-4 items-center">
+              <img src="/Images/phone.png" alt="" />
+              <h1 className="uppercase text-[#EF6D58] font-poppins font-normal leading-8 text-base">
+                Phone
+              </h1>
+            </div>
+            <h1 className="ml-[48px] mt-[26px] font-poppins leading-8 font-normal text-xl text-[#391400]">
+              +62 812 3456 7890
             </h1>
           </div>
-          <h1 className="ml-[48px] mt-[26px] font-poppins leading-8 font-normal text-xl text-[#391400]">
-            +1 567-89-00
-          </h1>
         </div>
-      </div>
-      <div className="w-[391px] h-[192px] border-[1px] border-second">
-        <div className="flex flex-col">
+        <div className="w-[391px] h-[192px] border-[1px] border-second">
+          <div className="flex flex-col">
             <div className="flex ml-[48px] mt-[32px] gap-4 items-center">
               <img src="/Images/messagepng.png" alt="" />
               <h1 className="uppercase text-[#EF6D58] font-poppins font-normal leading-8 text-base">
@@ -25,12 +26,12 @@ const Footer = () => {
               </h1>
             </div>
             <h1 className="ml-[48px] mt-[26px] font-poppins leading-8 font-normal text-xl text-[#391400]">
-              info@agency.com
+              sean@gmail.com
             </h1>
           </div>
-      </div>
-      <div className="w-[391px] h-[192px] rounded-r-lg border-[1px] border-second">
-        <div className="flex flex-col">
+        </div>
+        <div className="w-[391px] h-[192px] rounded-r-lg border-[1px] border-second">
+          <div className="flex flex-col">
             <div className="flex ml-[48px] mt-[32px] gap-4 items-center">
               <img src="/Images/address.png" alt="" />
               <h1 className="uppercase text-[#EF6D58] font-poppins font-normal leading-8 text-base">
@@ -38,12 +39,12 @@ const Footer = () => {
               </h1>
             </div>
             <h1 className="ml-[48px] mt-[26px] font-poppins leading-8 font-normal text-xl text-[#391400]">
-              2247 Lunetta Street, TX 76301
+              Surabaya
             </h1>
           </div>
+        </div>
       </div>
-    </div>
-  {/* <div className="flex">
+      {/* <div className="flex">
     <h1 className="">
       Agency
     </h1>
@@ -58,7 +59,8 @@ const Footer = () => {
       </div>
     </div>
   </div> */}
-  </div>;
+    </div>
+  );
 };
 
 export default Footer;
